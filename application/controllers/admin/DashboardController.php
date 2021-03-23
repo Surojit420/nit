abdd1e2eb9e4ee5fd87e59f0c2baee04b3f7f79d
BabuSona2020 <bcaprogramup@gmail.com>
2021-03-23 12:23:17 +0530

    babu 23032021 12:23

--- application/controllers/admin/DashboardController.php
+++ application/controllers/admin/DashboardController.php
@@ -21,6 +21,24 @@ class DashboardController extends CI_Controller
 		$this->data['subview']='dashboard/dashboard';
 		$this->data['logo_icons']=$this->CommonModel->RetriveRecordByWhereRow('tbl_logo',['status'=>'Active'],'image');
 		$this->data['foot_con'] = $this->CommonModel->RetriveRecordByWhereRow('tbl_contact',['status'=>'Active'],'footer_copy_right');
+		$this->data['logo_count']=$this->CommonModel->CountWhere('tbl_logo',['status' => 'Active']);
+		$this->data['banner_count']=$this->CommonModel->CountWhere('tbl_banner',['status'=>'Active']);
+		$this->data['business_count']=$this->CommonModel->CountWhere('tbl_business',['status'=>'Active']);
+		$this->data['job_apply_count']=$this->CommonModel->CountWhere('tbl_job_apply',['status'=>'Active']);
+		$this->data['job_summary_count']=$this->CommonModel->CountWhere('tbl_job_summary',['status'=>'Active']);
+		$this->data['contact_count']=$this->CommonModel->CountWhere('tbl_contact',['status'=>'Active']);
+		$this->data['mission_count']=$this->CommonModel->CountWhere('tbl_mission',['status'=>'Active']);
+		$this->data['portfolio_count']=$this->CommonModel->CountWhere('tbl_portfolio',['status'=>'Active']);
+		$this->data['query_count']=$this->CommonModel->CountWhere('tbl_query',['status'=>'Active']);
+		$this->data['service_count']=$this->CommonModel->CountWhere('tbl_services',['status'=>'Active']);
+		$this->data['service_type_count']=$this->CommonModel->CountWhere('tbl_services_type',['status'=>'Active']);
+		$this->data['technologies_count']=$this->CommonModel->CountWhere('tbl_technologies',['status'=>'Active']);
+		$this->data['vision_count']=$this->CommonModel->CountWhere('tbl_vision',['status'=>'Active']);
+		$this->data['why_choose_count']=$this->CommonModel->CountWhere('tbl_why_choose',['status'=>'Active']);
+		$this->data['work_flow_count']=$this->CommonModel->CountWhere('tbl_work_flow',['status'=>'Active']);
+		
+
+
 		$this->load->view('admin/layout/default', $this->data);
 	}
 
